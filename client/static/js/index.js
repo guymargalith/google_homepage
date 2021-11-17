ed54515b826771851ab4267c49dc79bddcd317b6
EmilyKral <emilykral@gmail.com>
2021-11-17 15:19:18 +0000

    added functionality to i'm feeling lucky button

---
+++ client/static/js/index.js
@@ -0,0 +1,20 @@
+const luckyButton = document.querySelector("#lucky-button");
+const mainSearch = document.querySelector("#main-search");
+
+luckyButton.addEventListener("click", feelingLucky);
+
+function feelingLucky(e) {
+	e.preventDefault();
+	let searchTerm = mainSearch.value;
+	console.log(searchTerm);
+	if (searchTerm.toLowerCase() === "pingu") {
+		searchTerm = "pingu";
+	} else {
+		searchTerm = "javascript";
+	}
+	fetch(`http://localhost:3000/search/${searchTerm}/random`)
+		.then(response => response.json())
+		.then(data => {
+			window.location = data.url;
+		});
+}
